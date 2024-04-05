c062cd60a057281e82532b65f97ecb8269a5b6bd
PUNCHAN0M <130173517+PUNCHAN0M@users.noreply.github.com>
2024-04-05 11:41:03 +0700

    defind color theme

--- openapi/tailwind.config.ts
+++ openapi/tailwind.config.ts
@@ -7,6 +7,31 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      // green
+      'green-500':'#3F979B',
+      'green-200' : '#B9F8BF', 
+
+      //blue
+      'blue-500' : '#1A73E8',
+      'blue-400' : '#3CA3DD',
+      'blue-300' : '#98D0EF',
+      'blue-100' : '#C2FCFF',
+
+      //yellow
+      'yellow-200' : '#F6F6CD',
+      'yellow-100' : '#FCFFDB',
+
+      //black  
+      'black-500' : '#000000',
+      'black-300' : '#424242',
+      'black-200' : '#222222',
+
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
