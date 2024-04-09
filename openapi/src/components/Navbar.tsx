0d0351f4465152568eb68f89241260e9d96197f6
PUNCHAN0M <130173517+PUNCHAN0M@users.noreply.github.com>
2024-04-09 16:43:26 +0700

    update navbar

--- openapi/src/components/Navbar.tsx
+++ openapi/src/components/Navbar.tsx
@@ -1,8 +1,20 @@
 const Navbar = () => {
-    return (
-        <nav>
-            <div className="bg-white h-16 border-b-[1px] border-grey-200 blur-[1px] " ></div>
-        </nav>
-    )
-}
+  return (
+    <nav className="flex bg-white h-16 border-b-[1px] border-grey-200 border-opacity-50 justify-between">
+      {/* front */}
+      <div className="flex items-center m-5"></div>
+      {/* back */}
+      <div className="flex ">
+        <ul className="flex flex-row">
+          <li className="bg-green-500 text-white m-5 px-5 rounded-full">
+            Login
+          </li>
+          <li className="bg-blue-100 text-black m-5 px-5  rounded-full">
+            Sign Up
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
