141fe2845eff16d8da5376a46214940021531a9a
PUNCHAN0M <130173517+PUNCHAN0M@users.noreply.github.com>
2024-04-13 17:18:42 +0700

    fix color login and fix size icon

---
+++ openapi/src/pages/login/index.tsx
@@ -0,0 +1,29 @@
+import LoginForm from "@/components/Login-registerForm/LoginForm";
+import Navbar from "@/components/Navbar";
+import SvgSignUp from "@/components/SVG/SvgSignUp";
+import { loginSchema } from "@/modules/login-register/login";
+import { useState } from "react";
+
+export default function login() {
+  // const [formLogin, setFormLogin] = useState<formTextSchema>();
+
+  const handleSubmitLogin = (value: loginSchema): void => {
+    // setFormLogin(value);
+    // console.log(value);
+  };
+  return (
+    <div className="flex  w-screen h-screen">
+      <div className="flex flex-col bg-white rounded-3xl shadow-lg  w-[768px] h-[489px] m-auto justify-center items-center">
+        <div className="flex flex-col bg-white items-center">
+          <div className="flex bg-gray-10  rounded-full w-[40px] h-[40px] justify-center items-center">
+            <SvgSignUp />
+          </div>
+          <div>เข้าสู่ระบบ</div>
+          <div>กรอกบัญชีอีเมลและรหัสผ่านเพื่อเข้าสู่ระบบ</div>
+          <div>_____________</div>
+        </div>
+        <LoginForm submitLogin={handleSubmitLogin} />
+      </div>
+    </div>
+  );
+}
