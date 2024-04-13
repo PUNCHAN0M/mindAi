87a14ded336afe109d98f0219a1cb394668182f7
PUNCHAN0M <130173517+PUNCHAN0M@users.noreply.github.com>
2024-04-13 16:15:14 +0700

    complete processbar in create.tsx

--- openapi/src/pages/create/index.tsx
+++ openapi/src/pages/create/index.tsx
@@ -1,24 +1,91 @@
-import ConfirmPost from "@/components/ConfirmPost";
-import FormProject from "@/components/Form";
-import Navbar from "@/components/Navbar";
-import ListTeacher from "@/components/RecommendTeacher";
-import { formSchema } from "@/modules/form";
+import clsx from "clsx";
 import { useState } from "react";
 
-export default function Book() {
-  const [formFormik, setFormFormik] = useState<formSchema>();
+export default function create() {
+  const [handleNextBtn, sethandleNextBtn] = useState<number>(0);
+  const [handlePreviousBtn, setHandlePreviousBtn] = useState<number>(0);
+  const handlePrevious = () => {
+    setHandlePreviousBtn(handlePreviousBtn - 1);
+  };
 
-  const handleSubmit = (value: formSchema): void => {
-    setFormFormik(value);
-    console.log(value);
+  const handleNext = () => {
+    setHandlePreviousBtn(handlePreviousBtn + 1);
   };
   return (
-    <div>
-      <Navbar/>
-      <FormProject submit={handleSubmit} />
-      <ListTeacher/>
-      <ConfirmPost/>
+    <div className="flex flex-col bg-white w-screen h-screen">
+      {/* <Navbar /> */}
+      <div className="flex px-5 w-full justify-center">
+        <div className="flex  items-center gap-x-[150px] justify-center">
+          <div
+            className={clsx(
+              "flex bg-grey-200 h-[3px] w-[450px]  mb-5 absolute "
+            )}
+          ></div>
+          <div
+            className={clsx(
+              "flex bg-blue-400 h-[3px]  mb-5  absolute ",
+              handlePreviousBtn === 1 ? "w-[220px] items-start mr-[200px]" : "",
+              handlePreviousBtn === 2 ? "w-[450px]" : ""
+            )}
+          ></div>
+          <div className="flex flex-col items-center w-[70px]">
+            <div className="flex bg-white w-[24px] h-[24px] rounded-full border-[3px] border-blue-400 text-center absolute justify-center items-center">
+              1
+            </div>
+            <div className="flex text-xs mt-7">กรอกข้อมูล</div>
+          </div>
+          <div className="flex flex-col items-center w-[70px]">
+            <div
+              className={clsx(
+                "flex bg-white w-[24px] h-[24px] rounded-full border-[3px] text-center  absolute justify-center items-center ",
+                handlePreviousBtn === 1 || handlePreviousBtn === 2
+                  ? "border-blue-400"
+                  : "border-grey-200"
+              )}
+            >
+              2
+            </div>
+            <div className="flex text-xs mt-7">แนะนำ</div>
+          </div>
+          <div className="flex flex-col items-center w-[70px]">
+            <div
+              className={clsx(
+                "flex  bg-white w-[24px] h-[24px] rounded-full border-[3px] text-center absolute justify-center items-center ",
+                handlePreviousBtn === 2 ? "border-blue-400" : "border-grey-200"
+              )}
+            >
+              3
+            </div>
+            <div className="flex text-xs mt-7">ยืนยัน</div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={clsx(
+          "flex flex-row justify-end",
+          handlePreviousBtn === 0 ? "justify-end" : "justify-between"
+        )}
+      >
+        <button
+          className={clsx(
+            "bg-grey-100 rounded-full",
+            handlePreviousBtn === 0 ? "hidden" : ""
+          )}
+          onClick={handlePrevious}
+        >
+          ย้อนกลับ
+        </button>
+        <button
+          className={clsx(
+            "bg-yellow-100 rounded-full",
+            handlePreviousBtn === 2 ? "hidden" : ""
+          )}
+          onClick={handleNext}
+        >
+          ถัดไป
+        </button>
+      </div>
     </div>
   );
 }
-
