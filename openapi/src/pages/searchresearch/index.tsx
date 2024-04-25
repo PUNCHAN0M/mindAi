1088244f2a03163f96c9b634be50a496b9add2cb
PUNCHAN0M <130173517+PUNCHAN0M@users.noreply.github.com>
2024-04-25 19:36:20 +0700

    update path

---
+++ openapi/src/pages/searchresearch/index.tsx
@@ -0,0 +1,27 @@
+import FormText from "@/components/Form/FormText";
+import Navbar from "@/components/navbar";
+import { useState } from "react";
+import Sidebar from "@/components/Sidebar";
+import {formTextSchema } from "@/modules/form";
+import ListTeacher from "@/components/Recommend/ListTeacher";
+import ProjectDetail from "@/components/Confirm/ProjectDetail";
+import ShowDetail from "@/components/Confirm/ShowDetail";
+
+export default function create() {
+
+  const handleSubmit = (values: formTextSchema): void => {
+
+    console.log(values);
+  };
+  return (
+    
+    <div className="flex flex-col bg-white w-screen h-screen">
+       <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet" />
+      <Navbar />
+      <FormText submit={handleSubmit} />
+
+      
+    </div>
+  );
+}
