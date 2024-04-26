a852ccd48aeb98db72632be6e30c66811f0e9419
PUNCHAN0M <130173517+PUNCHAN0M@users.noreply.github.com>
2024-04-26 15:01:13 +0700

    add oidc

---
+++ openapi/src/pages/oidc.ts
@@ -0,0 +1,29 @@
+export interface OIDCConfig {
+    onSigninCallback: (user: any) => void;
+    onSignoutCallback: (user: any) => void;
+    authority: string;
+    client_id: string;
+    scope: string;
+    response_type: string;
+    silent_renew: boolean;
+    use_refresh_token: boolean;
+    redirect_uri: string;
+}
+
+const oidcConfig: OIDCConfig = {
+    onSignoutCallback: (user) => {
+        window.location.href = '/';
+    },
+    onSigninCallback: (user) => {
+        window.location.href = '/searchresearch';
+    },
+    authority: '',//http://psusso-test.psu.ac.th/application/o/psuapi-contest-chanom
+    client_id: '',//xUnSLmLZwZ0bWPVyUvfWsnJHbSIjKUGId6efo2RR
+    scope: 'openid email profile offline_access',
+    response_type: 'code',
+    silent_renew: true,
+    use_refresh_token: true,
+    redirect_uri: 'http://localhost:3000/dashboard',
+};
+
+export default oidcConfig;
