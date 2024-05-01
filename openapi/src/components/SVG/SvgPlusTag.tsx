53e6e41ebd914878f7007e4d68bfa43f7029c722
PUNCHAN0M <130173517+PUNCHAN0M@users.noreply.github.com>
2024-05-01 20:07:25 +0700

    add tags

---
+++ openapi/src/components/SVG/SvgPlusTag.tsx
@@ -0,0 +1,17 @@
+const SvgPlusTags = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="2"
+    stroke="#222222"
+    className="w-[1.1vw] h-[1.1vw] mr-[1vh]"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 4.5v15m7.5-7.5h-15"
+    />
+  </svg>
+);
+export default SvgPlusTags;
