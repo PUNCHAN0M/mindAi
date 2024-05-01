5223fddd100bdbbf1e411ac00779e57d101401af
Sitta Sahathum <sittasahathum@gmail.com>
2024-05-01 16:35:36 +0700

    add sidebar

--- openapi/src/components/Sidebar.tsx
+++ openapi/src/components/Sidebar.tsx
@@ -1,11 +1,65 @@
-import SvgBars from "./SVG/SvgBars";
+import SvgHomePage from "./SVG/sidebar/SvgHomePage"
+import SvgSearch from "./SVG/sidebar/SvgSearch";
+import SvgCreateProject from "./SVG/sidebar/SvgCreateProject"
+import SvgAccount from "./SVG/sidebar/SvgAccount"
+import SvgNoti from "./SVG/sidebar/SvgNoti"
 
  const Sidebar = () => {
     return (
-        <div className="flex w-[4vw] h-screen bg-green-500">
-            <SvgBars/>
+            <div className="sidebar">
+                <nav className="sidebar-nav">
+                <ul>
+                    <div className="sidebar-top">
+                    <li>
+                        <a href="/dashboard" className="list-sidebar">
+                            <div className="icon-container">
+                                <i className="icon-sidebar"><SvgHomePage className ="svghomepage"/></i>
+                                <span className="nav-item">หน้าหลัก</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/searchresearch" className="list-sidebar">
+                            <div className="icon-container">
+                                <i className="icon-sidebar"><SvgSearch className="svgsearch"/></i>
+                                <span className="nav-item">ค้นหา</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/createprojectstudent" className="list-sidebar">
+                            <div className="icon-container">
+                                <i className="icon-sidebar"><SvgCreateProject className="svgcreateproject"/></i>
+                                <span className="nav-item">เพิ่มงานวิจัย</span>
+                            </div>
+                        </a>
+                    </li>
+                    </div>
+                    <hr className="line-sidebar" />
+                    <div className="sidebar-bottom">
+                    <li>
+                        <a href="#" className="list-sidebar">
+                            <div className="icon-container">
+                                <i className="icon-sidebar"><SvgAccount className="svgaccount"/></i>
+                                <span className="nav-item">ข้อมูลบัญชี</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="list-sidebar">
+                            <div className="icon-container">
+                                <i className="icon-sidebar"><SvgNoti className="svgnoti"/></i>
+                                <span className="nav-item">การแจ้งเตือน</span>
+                            </div>
+                        </a>
+                    </li>
+                    </div>
+                </ul>
+                </nav>
+
             
-        </div>
+            </div>
+            
     )
  }
  export default Sidebar;
--- openapi/src/pages/searchresearch/index.tsx
+++ openapi/src/pages/searchresearch/index.tsx
@@ -19,6 +19,7 @@ export default function create() {
   return (
     
     <div className="flex flex-col bg-white w-screen h-screen overflow-hidden">
+      <Sidebar/>
        <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet" />
       <Navbar />
