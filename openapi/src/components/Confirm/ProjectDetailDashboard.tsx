6d02dbf6cac6766374c80a797aa3a5822b1d7042
Sitta Sahathum <sittasahathum@gmail.com>
2024-05-02 17:42:05 +0700

    Pull Pun

--- openapi/src/components/Confirm/ProjectDetailDashboard.tsx
+++ openapi/src/components/Confirm/ProjectDetailDashboard.tsx
@@ -205,7 +205,7 @@ const ProjectDetailDashboard = () => {
           <hr className="line-visiblepost" />
         </div>
         <div className="create-researchcontainr">
-          <div className="name-researchcreate">สร้างโพสต์งานวิจัย</div>
+          <div className="name-researchcreate"><a href="/createprojectstudent">สร้างโพสต์งานวิจัย</a></div>
           <div className="icon-researchcreate">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
